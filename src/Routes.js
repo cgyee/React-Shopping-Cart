f18e127e0ba25509dbb3d98c5b052d95fbafb004
cgyee <cyee2017@fau.edu>
2021-01-09 02:51:27 -0500

    first commit

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import App from "./App";
+import Profile from "./Profile";
+
+const Routes = () => {
+    return(
+        <BrowserRouter> 
+            <Switch>
+                
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes
